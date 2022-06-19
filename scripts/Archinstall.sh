ec606194617364d1ab829fa1538def5fe27e13a0
Jovic Gutierrez <yubikkun@gmail.com>
2022-06-19 11:31:38 +0800

    updated Archinstall script

--- scripts/Archinstall.sh
+++ scripts/Archinstall.sh
@@ -2,4 +2,8 @@
 
 # ArchLinux Installation script for base with rice software
 
-sudo pacman -S i3 feh picom terminator xdg-user-dirs
+sudo pacman -S i3-gaps zsh feh picom terminator xdg-user-dirs
+
+# install zsh
+chsh -s /bin/zsh
+homectl update --shell=/bin/zsh biku
