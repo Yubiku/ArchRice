22a2521b166928a210677b9eef84b353e4caf018
Jovic Gutierrez <jovicgutierrez@proton.me>
2022-06-24 14:11:06 +0800

    update script

--- scripts/Archinstall.sh
+++ scripts/Archinstall.sh
@@ -2,8 +2,14 @@
 
 # ArchLinux Installation script for base with rice software
 
-sudo pacman -S i3-gaps zsh feh picom terminator xdg-user-dirs
+sudo pacman -S base-devel i3-gaps zsh feh picom terminator pcmanfm xdg-user-dirs
+
+# create user
+useradd -m -g wheel biku
+sudo sed '88s/^#//' /etc/sudoers
 
 # install zsh
 chsh -s /bin/zsh
 homectl update --shell=/bin/zsh biku
+
+
