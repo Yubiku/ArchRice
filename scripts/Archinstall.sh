e2318ce7ab14d7c053406aca39878b0926d47296
Jovic Gutierrez <jovicgutierrez@proton.me>
2022-06-30 03:30:41 +0000

    Added Zsh Plugin from repo

--- scripts/Archinstall.sh
+++ scripts/Archinstall.sh
@@ -2,7 +2,7 @@
 
 # ArchLinux Installation script for a Minimal i3 Setup
 
-sudo pacman -S base-devel alsa-utils i3-gaps i3-blocks zsh feh picom arandr terminator pcmanfm xdg-user-dirs
+sudo pacman -S base-devel alsa-utils i3-gaps i3-blocks zsh zsh-autosuggestions zsh-syntax-highlighting feh picom arandr terminator pcmanfm xdg-user-dirs
 
 # create user
 useradd -m -g wheel biku
